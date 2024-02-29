28feaba5a9644aa1276c8de8bb19c443cbddc4c6
Joseph Lee <joseph@teleosmarketing.com>
2024-02-29 11:02:07 -0600

    add enumeration of ad categories

---
+++ blog-ads-manager.js
@@ -0,0 +1,21 @@
+const AD_CATEGORIES = {
+    DME: "DME",
+    NPWT: "NPWT",
+    OXYGEN: "OXYGEN",
+    CONCENTRATORS: "CONCENTRATORS",
+    PREVENTATIVE_MAINTENANCE: "PREVENTATIVE_MAINTENANCE"
+}
+
+const CTA_URLS = {
+    DME: "",
+    OXYGEN: "https://masvidahealth.com/easy-quote-landing-page-oxygen/",
+    CONCENTRATORS: "https://masvidahealth.com/concentrator-easy-quote/",
+    PREVENTATIVE_MAINTENANCE: "https://masvidahealth.com/quote-concentration-preventative-maintance/",
+}
+
+var ad_data = {
+  1: {
+    gif_source: "https://masvidahealth.com/wp-content/uploads/2024/02/Spot-Ad-Concepts-Fast-DME.gif",
+    category: AD_CATEGORIES.DME,
+  }  
+};
